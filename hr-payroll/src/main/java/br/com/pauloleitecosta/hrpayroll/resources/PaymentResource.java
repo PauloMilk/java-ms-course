a3a1ffaf811205c478cde4936e17b52ebd8abdd8
Paulo Leite <pauloleitecosta14@gmail.com>
2021-06-03 14:02:21 -0300

    Hystrix faillback

--- hr-payroll/src/main/java/br/com/pauloleitecosta/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/br/com/pauloleitecosta/hrpayroll/resources/PaymentResource.java
@@ -7,8 +7,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 import br.com.pauloleitecosta.hrpayroll.entities.Payment;
 import br.com.pauloleitecosta.hrpayroll.services.PaymentService;
+
 
 @RestController
 @RequestMapping(value = "/payments")
@@ -17,10 +20,16 @@ public class PaymentResource {
 	@Autowired
 	private PaymentService service;
 	
+	@HystrixCommand(fallbackMethod = "getPaymentAlternative")
 	@GetMapping(value="/{workerId}/days/{days}")
 	public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days) {
 		Payment payment = service.getPayment(workerId, days);		
 		return ResponseEntity.ok(payment);
+	}
+	
+	public ResponseEntity<Payment> getPaymentAlternative( Long workerId,  Integer days) {
+		Payment payment = new Payment("Brann", 400.0, days);
+		return ResponseEntity.ok(payment);		
 	}
 
 }
