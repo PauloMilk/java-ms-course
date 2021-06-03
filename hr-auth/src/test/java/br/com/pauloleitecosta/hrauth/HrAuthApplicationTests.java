0bfe0c8e8186bb49cf065935aa25e3d9721f4b33
Paulo Leite <pauloleitecosta14@gmail.com>
2021-06-03 18:32:28 -0300

    Project hr-auth created

---
+++ hr-auth/src/test/java/br/com/pauloleitecosta/hrauth/HrAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pauloleitecosta.hrauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
